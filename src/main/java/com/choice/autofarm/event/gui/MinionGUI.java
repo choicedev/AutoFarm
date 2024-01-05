a91b2b6c7959823cf45f30baf1917e9e38a2d823
choicedev <fabricio_zamora1@outlook.com>
2024-01-05 07:25:55 -0300

    start minion gui

---
+++ src/main/java/com/choice/autofarm/event/gui/MinionGUI.java
@@ -0,0 +1,13 @@
+package com.choice.autofarm.event.gui;
+
+import io.github.rysefventory.plugin.content.InventoryContents;
+import io.github.rysefoxx.inventory.plugin.content.InventoryProvider;
+import org.bukkit.entity.Player;
+
+public class MinionGUI implements InventoryProvider {
+
+    @Override
+    public void init(Player player, InventoryContents contents) {
+        InventoryProvider.super.init(player, contents);
+    }
+}

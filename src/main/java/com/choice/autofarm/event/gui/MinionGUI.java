38f1deee871e9414c8f4498e9ec562a32eab08ea
choicedev <fabricio_zamora1@outlook.com>
2024-01-05 07:42:32 -0300

    parado para criar config do minion

--- src/main/java/com/choice/autofarm/event/gui/MinionGUI.java
+++ src/main/java/com/choice/autofarm/event/gui/MinionGUI.java
@@ -3,11 +3,13 @@ package com.choice.autofarm.event.gui;
 import io.github.rysefoxx.inventory.plugin.content.InventoryContents;
 import io.github.rysefoxx.inventory.plugin.content.InventoryProvider;
 import org.bukkit.entity.Player;
+import org.mineacademy.fo.menu.model.ItemCreator;
+import org.mineacademy.fo.remain.CompMaterial;
 
 public class MinionGUI implements InventoryProvider {
 
     @Override
     public void init(Player player, InventoryContents contents) {
-        InventoryProvider.super.init(player, contents);
+        contents.fillBorders(ItemCreator.of(CompMaterial.BLACK_STAINED_GLASS_PANE).make());
     }
 }
